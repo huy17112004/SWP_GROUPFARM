1cfe7d013b39fa0df80c82509a3185e8c72f814c
hieukx <hieukimxuan@gmail.com>
2025-06-17 21:49:43 +0700

    Login (#6)

---
+++ src/main/java/dto/LoginResponseDTO.java
@@ -0,0 +1,16 @@
+package dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String message;
+    private boolean success;
+    private String accountType; // ADMIN, SALER, MANAGER, SHIPPER, WAREHOUSE_MANAGER, WHOLESALE_CUSTOMER
+    private long accountId;
+    private String username;
+    private String name; // Tên người dùng (nếu có)
+}
